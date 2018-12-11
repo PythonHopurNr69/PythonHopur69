46f174ab513f17b81f9b68c4c5163d37749f25fa
hartmanningvarsson <svuntutussa@gmail.com>
2018-12-11 13:19:08 +0000

    Update movement

--- Main.py
+++ Main.py
@@ -14,16 +14,18 @@ def maingame():
   pygame.display.update()
   while gameExit == False: 
     for event in pygame.event.get():
+      print(event)
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_q:
           gameExit = True
-        if event.key == pygame.K_LEFT:
-           x_cord = x_cord - 10
+        if event.key == pygame.K_LEFT and x_cord > 50:
+           x_cord = x_cord - 50
            moveMent(x_cord, y_cord, 10)
-        if event.key == pygame.K_RIGHT:
-            x_cord = x_cord + 10
+        if event.key == pygame.K_RIGHT and x_cord < 750:
+            x_cord = x_cord + 50
             moveMent(x_cord, y_cord, 10)
     pygame.draw.rect(gameDisplay,black, [10,10,10,10])
+    gameDisplay.fill(white)
   
 
 def moveMent(x, y, size):
