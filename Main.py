2d9150639171633cfc95d484cc3cbf4baddba1bf
stebbi <stefant17@ru.is>
2018-12-11 16:36:26 +0000

    added play again? funtionality

--- Main.py
+++ Main.py
@@ -11,14 +11,16 @@ yellow = (255, 255, 0)
 gameDisplay = pygame.display.set_mode((800,600))
 list_Enemies = []
 shots_moving = []
+font = pygame.font.SysFont(None, 25)
 
   
 def maingame():
+  list_Enemies.clear()
+  shots_moving.clear()
   x_cord = 67
   y_cord = 540 
   enemy_cord_x = random.randint(50, 750)
   enemy_cord_y = 20
-
   list_Enemies.append([enemy_cord_x, enemy_cord_y])
   gameExit = False 
   gameDisplay.fill(white)
@@ -40,14 +42,16 @@ def maingame():
             shots_moving.append([x_cord, y_cord])
     gameDisplay.fill(white)
     clock.tick(30)
-    for i in list_Enemies:
-        i[1] += 2.5
 
     for i in shots_moving:
         i[1] -= 3.6
 
     moveMent(x_cord, y_cord, 10, black)
     pygame.display.update()
+    for i in list_Enemies:
+        i[1] += 10
+        if i[1]> 650:
+          gameExit = gameOver()
 
 def detectCollision(enemies, shots):
     enemies = list_Enemies
@@ -79,6 +83,24 @@ def displayShots(lisMoving):
 def displayEnemies(lisEnemies):
     for i in lisEnemies:
         pygame.draw.rect(gameDisplay, red, [i[0], i[1], 20, 20])
+
+def gameOver():
+  messageToScreen('YOU LOSE!, PLAY AGAIN? (Y/N)')
+  pygame.display.update()
+  while 1:
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_y:
+          maingame()
+          return True
+        elif event.key == pygame.K_n:
+          return True
+
+def messageToScreen(msg):
+  message = font.render(msg,True, red)
+  gameDisplay.blit(message,[400,300])
+
+
 
 maingame()
 
