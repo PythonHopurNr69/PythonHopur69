fc7172c6ee6c98a792db1f82624c3fcbe0783b58
hartmanningvarsson <svuntutussa@gmail.com>
2018-12-12 14:37:01 +0000

    Update, working on buldscript

---
+++ Setup.py
@@ -0,0 +1,10 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("Main.py")]
+
+cx_Freeze.setup(
+    name="blockfudger",
+    options={"build_exe": {"packages":["pygame"]}},
+    executables = executables
+
+    )
