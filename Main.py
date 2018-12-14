cd7aceb3bb22c36434f56fcb9e855e24c5db71c4
stebbi <stefant17@ru.is>
2018-12-14 11:24:54 +0000

    updated README, and added Main.ba

--- Main.py
+++ Main.py
@@ -7,29 +7,54 @@ from score_repo import HighScores
 
 
 pygame.init()
+
+#pygame.time.clock is used to make the game feel more natural when playing (30 fps)
 clock = pygame.time.Clock()
+FPS = 30
+#setting up the colors before we use them
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 yellow = (255, 255, 0)
+
+#display width and height so we din't hard code the display. rather use a changable variable (can be changed while not changeing the core game)
 display_width = 800
 display_height = 600
+
+#movement for the main caracter (the player)
 jump_movement = 50
-spawn_enemy = pygame.USEREVENT  + 1
+
+#timer and set_timer is so an enemy spawnes every "timer" milliseconds (timer changes througout game check def raise_point)
+spawn_enemy = pygame.USEREVENT +1
 timer = 1000
+pygame.time.set_timer(pygame.USEREVENT +1, timer)
+
+#lvl_up is used to display how mutch points the player has to get to lvl up, and lvl is the current lvl
 lvl_up = 20
 lvl = 1
+
+#shot_speed and enemy_speed, determins the speed of the enemy and bullets, is changed throughout the game
 shot_speed = 3.5
 enemy_speed = 2.5
+
+#our displays, for our game and menu
 menuDisplay = pygame.display.set_mode((display_width,display_height))
 gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+# to create the enemies and shots we appended them into these lists and removed when destroyed or out of bounds
 list_Enemies = []
 shots_moving = []
+
+# list allowed space is so that an enemy will never appear in a place that the player can't shoot from
 list_allowed_space = []
+
+#our font for the text that appiers through out the game
 font = pygame.font.SysFont(None, 25)
 _highscores = HighScores()
 entry = Entry()
 
+#def initialize initializes everything we need in the main loop, 
+# (it's basicly reseting everything to make the game play from lvl 1)
 def initialize():
   global timer
   global list_Enemies
@@ -37,6 +62,8 @@ def initialize():
   global enemy_speed
   global lvl 
   global lvl_up
+  global points
+  points = 0
   lvl_up = 20
   lvl = 1 
   timer = 1000
@@ -51,9 +78,7 @@ def initialize():
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
-pygame.time.set_timer(pygame.USEREVENT +1, timer)
-  
-  
+#our game loop
 def maingame():
   initialize()
   x_cord = 67
@@ -66,47 +91,65 @@ def maingame():
   global points
   points = 0
   pygame.display.update()
+
+  #game loop starst
   while gameExit == False:
+
+    #the for "event" and if "event.type" takes the values of what the player has pressed on their keyboard
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_q:
+          #if the Q key is pressed, quit the game 
           gameExit = True
+        
         elif event.key == pygame.K_LEFT and x_cord > jump_movement:
-           x_cord = x_cord - jump_movement
-           moveMent(x_cord, y_cord, 10, black)
+          # if the left key is pressed, the player x-coordinates will move left for 50 pixles (jump_movement) if the player is not out of bounds
+          x_cord = x_cord - jump_movement
+          moveMent(x_cord, y_cord, 10, black)
+        
         elif event.key == pygame.K_RIGHT and x_cord < int(display_width-jump_movement):
-            x_cord = x_cord + jump_movement
-            moveMent(x_cord, y_cord, 10, black)
+          # if the right key is pressed the x-coordinates of the player will be moved right 50 pixles (jump_movemnt) if the player is not out of bounds
+          x_cord = x_cord + jump_movement
+          moveMent(x_cord, y_cord, 10, black)
+        
         elif event.key == pygame.K_SPACE:
-            fireShot(x_cord, y_cord, 2)
-            shots_moving.append([x_cord, y_cord])
-        elif event.key == pygame.K_e:
-          list_Enemies.append([(random.choice(list_allowed_space)), enemy_cord_y])
-      elif event.type == pygame.MOUSEBUTTONDOWN:
-        print(event)
+          #if the spacebar is pressed a shot will be appended into "shots moveing" containing the x and y coordinates of the shot 
+          fireShot(x_cord, y_cord, 2)
+          shots_moving.append([x_cord, y_cord])
       elif event.type == spawn_enemy:
+        # the timer that is set in initialize will send an event "spawn enemy" every "timer"-milliseconds, and that will trigger this to spawn the enemy
         add_enemy()
     gameDisplay.fill(white)
-    clock.tick(30)
+    #This makes the clock tick every "FPS"-per second, and makes the game run at 30 fps 
+    clock.tick(FPS)
 
+#this for loop changes the y coordinates of the shots to make them seem like they'r moving,
+# and if the y-coordinates is less then 0 then remove it from the list (so it doesn't get unnessiserily large)
     for i in shots_moving:
       i[1] -= shot_speed
       if i[1] < 0:
         shots_moving.remove(i)
     check_if_hit()
     displayPoints(points)
+
+# this for loop changes the y coordinates of every enemy to make it seem like they're moving 
+#and check if any one of them are touching the player and if that is true, the game is over
     for i in list_Enemies:
       i[1] += enemy_speed
+      #check if the player is hit
       if i[0] > x_cord and i[0] < x_cord +20 or i[0] + 10 > x_cord and i[0] + 10 < x_cord + 20:
         if i[1] > y_cord and i[1] < y_cord + 20 or i[1]+10 > y_cord and i[1] + 10 < y_cord+ 20:
           gameExit = gameOver()
+      #this if is if multiple shots hit the same target the game doesnt crash
       if i[1]> display_height:
         if i in list_Enemies:
           list_Enemies.remove(i)
     check_if_hit()
+    #and in the end displays the movement 
     moveMent(x_cord, y_cord, 10, black)
 #        gameExit = gameOver()
 
+#our main menu
 def mainMenu():
   menuDisplay.fill(black)
 
@@ -125,9 +168,9 @@ def mainMenu():
   ExitMessage = font.render("q --- Exit program", True, red)
   gameDisplay.blit(ExitMessage, [600, 400])
   pygame.display.update()
+  #this while loop checks every event that the player makes and if it't a certain event (for example: hits 3 on his/her keyboard) and does something
   while 1:
     for event in pygame.event.get():
-      print(event)
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_q:
           return pygame.quit()
@@ -137,9 +180,13 @@ def mainMenu():
           return maingame()
         elif event.key == pygame.K_3:
           how_to_play()
+
+
+#this displayes the insturctions on how to play 
 def how_to_play():
   message = font.render('in this game you play as a black box and your goal is to shoot the red boxes for points',True, red)
   gameDisplay.blit(message,[display_height/4,display_width/2])
+
 def displayHighScores():
   window = Tk()
   disp_scores = StringVar()
@@ -188,7 +235,8 @@ def commitHighScores():
       tkinter.messagebox.showinfo('Error, could not add scores')
 
   
-
+#check if hit is called either when an enemy has moved or when a shot has been moved to check if
+#an enemy was hit and has to be "deleted" from the enemy list and shot list
 def check_if_hit():
   for i in list_Enemies:
     for e in shots_moving:
@@ -198,9 +246,11 @@ def check_if_hit():
             shots_moving.remove(e)
             raisePoints()
 
+#add enemy adds an enemy from a set x-coordinate (0, at the top) and a random y-coordinate form a list of allowed y coordinates that the player can hit 
 def add_enemy():
   list_Enemies.append([random.choice(list_allowed_space),0])
-      
+
+#our def moveMent is called when the player moves a caracter or when an enemy moves
 def moveMent(x, y, size, color):
   gameDisplay.fill(white)
   displayPoints(points)
@@ -209,21 +259,26 @@ def moveMent(x, y, size, color):
   displayShots(shots_moving)
   pygame.display.update()
 
+#when a shot is fired, what position it should take
 def fireShot(startPos, endPos, w):
-    pygame.draw.circle(gameDisplay, yellow, [startPos + 5, endPos - 10],  10)
-    displayShots(shots_moving)
-    pygame.display.update()
+  pygame.draw.circle(gameDisplay, yellow, [startPos + 5, endPos - 10],  10)
+  displayShots(shots_moving)
+  pygame.display.update()
 
+#displays our shots, is called mainly in our def moveMent 
 def displayShots(lisMoving):
     for i in shots_moving:
         pygame.draw.rect(gameDisplay, red, [i[0], i[1], 5, 5])
 
+#def displayEnemies is used to display enemies and is primaraly used in def moveMent
 def displayEnemies(lisEnemies):
     for i in lisEnemies:
         pygame.draw.rect(gameDisplay, red, [i[0], i[1], 20, 20])
 
+# what to display when the game is over and the player loses
 def gameOver():
   messageToScreen('YOU LOSE!, PLAY AGAIN? (Y/N)')
+  # the for and if event in while loop takes an event from the player if he/she want's to continue or quit 
   while 1:
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
@@ -236,16 +291,18 @@ def gameOver():
           return True
 
 
+#messageToScreen is called with a string delimiter that contains the message that is supposed to be shown on screen
 def messageToScreen(msg):
   message = font.render(msg,True, red)
   gameDisplay.blit(message,[display_height/4,display_width/2])
   pygame.display.update()
 
+#displays the points in the left hand corner
 def displayPoints(points):
   scoreboard = font.render("Score {0}".format(points) + " lvl: " + str(lvl), 1, (0,0,0))
   gameDisplay.blit(scoreboard, (5, 10))
 
-
+# a function that is called when the points have been rased, lvls up and makes the game harder the longer it goes on
 def raisePoints():
     global lvl
     global points
@@ -254,6 +311,7 @@ def raisePoints():
     global shot_speed
     global enemy_speed
     points = points + 1
+    #the if statements here are to make the enemies spawn quicker, from 1000ms to 900ms and so on 
     if points > lvl_up:
       lvl += 1
       if timer > 600:
@@ -262,6 +320,7 @@ def raisePoints():
         timer -=100
       else:
         timer = (int(timer/2))+1
+      #when lvl 10 has been reached the enemy sped will reset to 2.5 but the enemie spawn timer will still be going, to keep the game fresh after lvl 10 
       if lvl == 10:
         enemy_speed =2.5
       lvl_up += 20
--- README.md
+++ README.md
@@ -6,4 +6,14 @@
 #Download python from python.org, use 3.61 or greater
 
 #Step 2
-#To install a pygame use pip tool
+#download pygame from this website: "https://www.pygame.org/download.shtml"
+#Then install pygame using pip tool, with this code in terminal "python3 -m pip install -U pygame --user"
+#and this can happen to some versions of visual studio code, but if the code doesn't seem to work you might need to add a bit of code to your visual studio code settings. (it´s becasue "pygame" is kind of old, and there has been new things added in vs code as "protection" but pygame is completely safe)
+#to accses the settings use "CTRL+SHIFT+P" and search "settings(json)" there you sould add the folowing code inside the {} brackets
+ 	"python.linting.pylintArgs": [
+"----extension-pkg-whitelist=1xml"
+]   
+#sorry for the inconvenience but this can happen in vscode
+
+#if you want to run our game in the terminal after you have installed pygame, you can doubble click the "Main.ba" text file in the directory or run "Main.ba" in terminal. (apperantly IDLE and pygame don't work well together )
+
